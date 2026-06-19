829dda5ab21cae69387add3d5358ce5fabd8b0f0
Ismael <ismaeljorge257@gmail.com>
2026-06-19 10:48:10 -0300

    fix: força o envio do arvore_binaria.h da pratica 09

---
+++ praticas/pratica09/arvore_binaria.h
@@ -0,0 +1,17 @@
+#ifndef ARVORE_BINARIA_H
+#define ARVORE_BINARIA_H
+
+#define TRUE 1
+#define FALSE 0
+
+typedef struct arvore_t Arvore;
+
+Arvore* arvore_criar();
+int arvore_inserir(Arvore* a, int valor);
+void arvore_exibir_pre_ordem(Arvore* a);
+void arvore_exibir_em_ordem(Arvore* a);
+void arvore_exibir_pos_ordem(Arvore* a); // Ajustado para cobrir a duplicata do roteiro
+int arvore_esta_vazia(Arvore* a);
+void arvore_destruir(Arvore* a);
+
+#endif
